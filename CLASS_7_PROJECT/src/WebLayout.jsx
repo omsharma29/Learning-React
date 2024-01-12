08309c1ef1b5aecfc85b930e8221daac28d703f4
omsharma29 <151984518+omsharma29@users.noreply.github.com>
2024-01-12 13:45:43 +0530

    react router basic

---
+++ CLASS_7_PROJECT/src/WebLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom'
+import Footer from './components/Footer/Footer'
+
+function WebLayout() {
+  return (
+    <div>
+    <Header/>,
+    <Outlet />,
+    <Footer />
+    </div>
+  )
+}
+
+export default WebLayout
